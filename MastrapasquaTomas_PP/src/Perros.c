e725c90983f21758755dd26cf587d10fb5d2b60b
TomasMastra <78211532+TomasMastra@users.noreply.github.com>
2021-10-14 00:14:54 -0300

    Add files via upload

---
+++ MastrapasquaTomas_PP/src/Perros.c
@@ -0,0 +1,109 @@
+/*
+ * Perros.c
+ *
+ *  Created on: 12 oct. 2021
+ *      Author: Tomas Mastra
+ */
+
+#ifndef PERROS_C_
+#define PERROS_C_
+
+#include <stdio.h>   //MASTRAPASQUA TOMAS D
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+#include "Estadia.h"
+#include "Perros.h"
+#include "Funciones.h"
+#include "Nexo.h"
+
+#define P 3
+
+
+
+void harcodearPerros(ePerro perro[], int len)
+{
+
+		int i;
+		int ids[] = {7000, 7001, 7002};
+		char nombres[][21] = {"Lobo", "Shelia", "Reina"};
+		char razas[][21] = {"Sharpei", "Golden", "Galgo"};
+		int edad[] = {2, 12, 13};
+
+		for(i=0; i<len; i++)
+		{
+			perro[i].id = ids[i];
+			strcpy(perro[i].nombre, nombres[i]);
+			strcpy(perro[i].raza, razas[i]);
+			perro[i].edad = edad[i];
+		}
+
+
+
+
+}
+
+void perro_listar(ePerro perro[], int lenPerro)
+{
+
+	int i;
+
+	lenPerro = P;
+
+	printf("Listado de perros:\n\n"
+				   "%-5s %-20s %-20s %-20s\n", "ID", "Nombre", "Raza", "Edad");
+
+	for(i=0;i<lenPerro;i++)
+	{
+
+		 perro_listarUno(perro[i]);
+
+
+	}
+
+
+
+}
+
+void perro_listarUno(ePerro perro)
+{
+
+	printf("%-5d %-20s %-20s %-20d\n", perro.id, perro.nombre, perro.raza, perro.edad);
+
+}
+
+void perro_calcularPromedioEdad(ePerro perro[], int lenPerro)
+{
+
+	float promedio;
+	int i;
+	int sumaEdades;
+
+	sumaEdades = 0;
+	promedio = 0;
+
+
+	for(i=0;i<lenPerro;i++)
+	{
+		if(perro[i].edad<20)
+		{
+
+		sumaEdades = sumaEdades + perro[i].edad;
+
+		}
+
+	}
+
+
+	promedio = sumaEdades/lenPerro;
+
+	printf("Promedio de edades de los perros: %f\n", promedio);
+
+}
+
+
+
+
+
+#endif /* PERROS_C_ */
---
+++ MastrapasquaTomas_PP/src/Perros.h
@@ -0,0 +1,39 @@
+/*
+ * Perros.h
+ *
+ *  Created on: 12 oct. 2021
+ *      Author: Tomas Mastra
+ */
+
+#ifndef PERROS_H_
+#define PERROS_H_
+
+#include <stdio.h>   //MASTRAPASQUA TOMAS D
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+typedef struct
+{
+
+	int id;
+	char nombre[21];
+	char raza[21];
+	int edad;
+
+
+}ePerro;
+
+void harcodearPerros(ePerro perro[], int len);//Esta funcion harcodea los perros en la posicion de i
+
+void perro_listar(ePerro perro[], int len);//Esta funcion llama a la funcion de listarUno con un for para que se muestren todos los perros
+void perro_listarUno(ePerro perro);//esta funcion muestra un perro, se usa para la funcion de mostar todos
+
+void perro_calcularPromedioEdad(ePerro perro[], int lenPerro);//Esta funcion suma las edades para buscar el promedio de sus edades
+
+
+
+
+
+
+#endif /* PERROS_H_ */
