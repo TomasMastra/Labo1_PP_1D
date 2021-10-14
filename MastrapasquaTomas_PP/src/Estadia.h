92f9c19f7d87ee060c244a9b8f2b57d0b07f5164
TomasMastra <78211532+TomasMastra@users.noreply.github.com>
2021-10-13 23:56:35 -0300

    Add files via upload

---
+++ MastrapasquaTomas_PP/src/Estadia.h
@@ -0,0 +1,65 @@
+/*
+ * Estadia.h
+ *
+ *  Created on: 12 oct. 2021
+ *      Author: Tomas Mastra
+ */
+
+#ifndef ESTADIA_H_
+#define ESTADIA_H_
+
+#include <stdio.h>   //MASTRAPASQUA TOMAS D
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+typedef struct
+{
+
+	int dia;
+	int mes;
+	int anio;
+
+}eFecha;
+typedef struct
+{
+
+	int id;
+	char nombreDuenio[21];
+	int telefonoContacto;
+	int idPerro;
+	eFecha fecha;
+
+	int isEmpty;
+
+}eEstadia;
+
+void estadia_harcodear(eEstadia estadia[], int len);
+void estadia_inicializar(eEstadia estadia[], int len);//Esta funcion inicializa el array de isEmpty en -1 (EMPTY)
+
+int validarIsEmpty(eEstadia estadia[], int len);//Esta funcion la uso en caso de que se haya ingresado la opcion 1 y se eliminen todos las estadias
+
+
+void estadia_pedirDatos(eEstadia estadia[], int i, int id);//Esta funcion pide los datos, el index lo busca la funcion de inicializar en el nexo.c
+int estadia_buscarLibre(eEstadia estadia[], int len);
+
+void estadia_confirmar(eEstadia estadia[], int i);//Esta funcion pide que introduzcas s si queres que se guarde lo que ingresaste, en caso de que lo hagas en isEmpty se cambia a 1;
+int estadia_buscarID(eEstadia estadia[], int len, int id);//Esta funcion busca un ID
+
+
+
+void estadia_cancelar(eEstadia estadia[], int len);//Esta funcio elimina una estadia
+
+void estadia_listarTodos(eEstadia estadia[], int len);//Esta funcion muestra todas las estadias, llama a la de listarUno
+void estadia_listarUno(eEstadia estadia);//Esta funcion muestra 1 estadia
+void estadia_ordenar(eEstadia estadia[], int len);/*Esta funcion ordena por fecha, primero comparo el año y si son iguales estadia de i es igual a la de j, en caso de que sean iguales se compara el mes y despues el dia.
+ * En caso de que sean todas las fechas igual, se compara el nombreDuenio
+ *
+ *
+ */
+
+void estadia_pedirFecha(eEstadia estadia[], int i);//Esta funcion pide las fechas, dia, mes y año
+
+
+
+#endif /* ESTADIA_H_ */
---
+++ MastrapasquaTomas_PP/src/MastrapasquaTomas_PP.c
@@ -0,0 +1,200 @@
+/*
+ ============================================================================
+ Name        : MastrapasquaTomas_PP.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>   //MASTRAPASQUA TOMAS D
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+
+#include "Estadia.h"
+#include "Perros.h"
+#include "Funciones.h"
+#include "Nexo.h"
+
+
+
+#define X 5
+#define P 3
+
+int main(void) {
+
+	setbuf(stdout, NULL);
+
+
+	 int flag = 0;
+	 int option;
+	 int len = X;
+	 int lenPerro = P;
+
+	 eEstadia estadia[X];
+	 ePerro perro[X];
+
+	 int id = 100000;
+
+
+
+
+
+
+
+		 do{
+			 getInt(&option, "-------------------------------------\n"
+		 							 "1. ALTA estadia\n"
+		 							 "2. BAJA estadia\n"
+		 							 "3. MODIFICACION estadia\n"
+		 							 "4. LISTADO perros\n"
+		 							 "5. LISTADO ordenado por precio\n"
+		 							 "6. PROMEDIO edad perros\n"
+		 							 "11. SALIR\n"
+		 							 "-------------------------------------\n",
+		 							 "Ingrese una opcion: "
+		 							 "-------------------------------------\n"
+		 							 "1. ALTA estadias\n"
+		 							 "2. BAJA estadias\n"
+		 							 "3. MODIFICACION estadias\n"
+		 							 "4. LISTADO estadias\n"
+		 							 "5. LISTADO perros\n"
+		 							 "6. PROMEDIO edad perros\n"
+		 							 "11. SALIR\n"
+		 							 "-------------------------------------\n"
+		 							 "Opcion invalida, reingrese: ", 1, 10);
+
+		 		switch(option)
+		 		{
+		 			case 1:
+
+
+
+		 					 flag = inicializar(estadia, perro, len, flag, id);
+
+		 					 id++;
+		 					//flag = 1;
+
+
+
+		 				break;
+
+
+
+		 			case 2:
+
+		 				if(flag ==1)
+		 				{
+
+		 				 estadia_cancelar(estadia, len);
+		 				}else
+		 				{
+
+				 			printf("Ingrese una estadia\n");
+
+		 				}
+
+
+
+
+
+
+
+
+					break;
+
+		 			case 3:
+
+
+		 				if(flag == 1)
+		 				{
+		 					 modificarEstadia(estadia, perro, len);
+		 				}else
+		 				{
+
+				 			printf("Ingrese una estadia\n");
+
+
+		 				}
+
+
+
+
+
+		 				break;
+
+
+		 			case 4:
+
+		 					if(flag == 1)
+		 					{
+		 					estadia_ordenar(estadia,  len);
+		 					}else
+		 					{
+
+					 			printf("Ingrese una estadia\n");
+
+
+		 					}
+
+
+
+		 				break;
+
+
+		 		case 5:
+
+		 			if(flag == 1)
+		 			{
+		 			harcodearPerros(perro,  len);
+		 			 perro_listar(perro, lenPerro);
+		 			}else
+		 			{
+
+			 			printf("Ingrese una estadia\n");
+
+
+		 			}
+
+
+
+
+
+
+
+
+		 			break;
+
+
+		 		case 6:
+
+		 			if(flag == 1)
+		 			{
+		 				harcodearPerros(perro,  len);
+		 				perro_calcularPromedioEdad(perro, lenPerro);
+		 			}else
+		 			{
+
+			 			printf("Ingrese una estadia\n");
+
+		 			}
+
+
+
+		 			break;
+
+
+
+		 		}
+
+
+
+
+
+		 	}while(option != 11);
+
+	return EXIT_SUCCESS;
+}
